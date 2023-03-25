bb27a65671e65705b66d920314d9d788b92c289a
Lenovo <neng@ds.dfci.harvard.edu>
2023-03-25 19:25:50 -0400

    modify code

--- src/RunMiniprot.py
+++ src/RunMiniprot.py
@@ -4,18 +4,18 @@ import shlex
 
 
 class MiniprotRunner:
-    def __init__(self, miniprot_execute_command, run_folder, config):
+    def __init__(self, miniprot_execute_command, config):
         self.miniprot_execute_command = miniprot_execute_command
-        self.run_folder = run_folder
         self.threads = config.threads
 
-    def run_miniprot(self, assembly_filepath, lineage_filepath, output_filename):
-        output_filepath = os.path.join(self.run_folder, output_filename)
+    def run_miniprot(self, assembly_filepath, lineage_filepath, output_dir):
+        output_filepath = os.path.join(output_dir, "miniprot_output.gff")
         fout = open(output_filepath, "w")
         miniprot_process = subprocess.Popen(shlex.split(
             "{} -I --outs=0.95 -t {} --gff {} {}".format(self.miniprot_execute_command, self.threads,
                             assembly_filepath, lineage_filepath, output_filepath)), stdout=fout, bufsize=8388608)
         miniprot_process.wait()
+        fout.close()
         return output_filepath
 
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--execute", type=str, help="Miniprot execute command", required=True)
-    parser.add_argument("-r", "--run_folder", type=str, help="Run folder", required=True)
+    parser.add_argument("-r", "--output_dir", type=str, help="Run miniprot folder", required=True)
     parser.add_argument("-a", "--assembly", type=str, help="Assembly file path", required=True)
     parser.add_argument("-l", "--lineage", type=str, help="Lineage file path", required=True)
     parser.add_argument("-t", "--threads", type=int, default=16, help="Number of threads to use")
@@ -32,6 +32,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     miniprot_execute_command = args.execute
-    run_folder = args.run_folder
-    miniprot_runner = MiniprotRunner(miniprot_execute_command, run_folder, args)
-    miniprot_runner.run_miniprot(args.assembly, args.lineage, "test.gff")
+    miniprot_runner = MiniprotRunner(miniprot_execute_command, args)
+    miniprot_runner.run_miniprot(args.assembly, args.lineage, args.output_dir)
