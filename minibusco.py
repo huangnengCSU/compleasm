78ed0079112681695afe2c8e89efd2db99e077df
nenghuang <neng@ds.dfci.harvard.edu>
2023-04-12 13:37:30 -0400

    DownloadLineage done

--- minibusco.py
+++ minibusco.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue May  7 10:50:00 2019
+Author: Neng Huang
+Email: neng@ds.dfci.harvard.edu
+Date: 2023 Apr 11
 """
 
 import os
+import argparse
 import hashlib
+import sys
 import tarfile
 import urllib.request
 
 
-
-### DownloadLineage.py
+### utils
 class Error(Exception):
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return self.value
+
+
+### DownloadLineage.py
 
 class URLError(OSError):
     def __init__(self, reason, filename=None):
@@ -29,6 +35,7 @@ class URLError(OSError):
     def __str__(self):
         return '<urlopen error %s>' % self.reason
 
+
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
@@ -36,11 +43,11 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 class Downloader:
     def __init__(self, download_dir=None):
         self.base_url = "https://busco-data.ezlab.org/v5/data/"
-        # TODO: is only eukaryota_odb10 ok?
-        self.default_lineage = ["archaea_odb10", "bacteria_odb10", "eukaryota_odb10"]
+        self.default_lineage = ["eukaryota_odb10"]
         if download_dir is None:
             self.download_dir = "mb_downloads"
         else:
@@ -56,20 +63,12 @@ class Downloader:
 
         self.lineage_description, self.placement_description = self.download_file_version_document()
 
-        print("Get file version description done.")
         self.download_placement()  # download placement files
-        for lineage in self.default_lineage:
-            try:
-                if self.check_lineage(lineage):
-                    pass
-                    # self.lineage_description[lineage].append(os.path.join(self.download_dir, lineage))
-                else:
-                    self.download_lineage(lineage)
-            except KeyError:
-                raise Error("invalid lineage name: {}".format(lineage))
 
-    @staticmethod
-    def download_single_file(remote_filepath, local_filepath, expected_hash):
+        for lineage in self.default_lineage:
+            self.download_lineage(lineage)
+
+    def download_single_file(self, remote_filepath, local_filepath, expected_hash):
         try:
             urllib.request.urlretrieve(remote_filepath, local_filepath)
             observed_hash = md5(local_filepath)
@@ -92,84 +91,112 @@ class Downloader:
         hash_url = self.base_url + "file_versions.tsv.hash"
         hash_download_path = os.path.join(self.download_dir, "file_versions.tsv.hash")
 
-        if not os.path.exists(hash_download_path):
+        if os.path.exists(file_version_download_path + ".tmp"):
+            sys.exit("file_versions.tsv.tmp exists, another process is downloading, please run again later.")
+
+        if not os.path.exists(file_version_download_path + ".done"):
+            open(file_version_download_path + ".tmp", 'w').close()
+
             # download hash file
             try:
                 urllib.request.urlretrieve(hash_url, hash_download_path)
             except URLError:
                 print("Cannot reach {}".format(hash_url))
-                raise Error("Unable to download necessary files")
-        expected_file_version_hash = ""
-        with open(hash_download_path, 'r') as fin:
-            expected_file_version_hash = fin.readline().strip()
+                os.remove(file_version_download_path + ".tmp")
+                raise Error("Unable to download necessary file {}".format("file_versions.tsv.hash"))
 
-        if os.path.exists(file_version_download_path):
-            download_success = True
-        else:
-            # download file version
-            download_success = self.download_single_file(file_version_url, file_version_download_path,
+            with open(hash_download_path, 'r') as fin:
+                expected_file_version_hash = fin.readline().strip()
+
+            download_success = self.download_single_file(file_version_url,
+                                                         file_version_download_path,
                                                          expected_file_version_hash)
+            if not download_success:
+                os.remove(file_version_download_path + ".tmp")
+                raise Error("Unable to download necessary file {}".format("file_versions.tsv"))
+            else:
+                open(file_version_download_path + ".done", 'w').close()
+                os.remove(file_version_download_path + ".tmp")
+
         lineages_description_dict = {}
         placement_description_dict = {}
-        if download_success:
-            with open(file_version_download_path, 'r') as fin:
-                for line in fin:
-                    strain, date, hash_value, category, info = line.strip().split()
-                    if info == "lineages":
-                        lineages_description_dict[strain] = [date, hash_value, category]
-                    elif info == "placement_files":
-                        placement_description_dict[strain] = [date, hash_value, category]
-            return lineages_description_dict, placement_description_dict
-        else:
-            return None, None
+        with open(file_version_download_path, 'r') as fin:
+            for line in fin:
+                strain, date, hash_value, category, info = line.strip().split()
+                if info == "lineages":
+                    lineages_description_dict[strain] = [date, hash_value, category]
+                elif info == "placement_files":
+                    placement_description_dict[strain] = [date, hash_value, category]
+        return lineages_description_dict, placement_description_dict
 
-    def check_lineage(self, lineage):
-        try:
-            if os.path.exists(os.path.join(self.download_dir, lineage)):
-                self.lineage_description[lineage].append(os.path.join(self.download_dir, lineage))
-                return True
-            else:
-                return False
-        except KeyError:
-            raise Error("invalid lineage name: {}".format(lineage))
+    def download_lineage(self, lineage):
+        if not lineage.endswith("_odb10"):
+            lineage = lineage + "_odb10"
+        if os.path.exists(os.path.join(self.download_dir, lineage) + ".tmp"):
+            sys.exit("{}.tmp exists, another process is downloading, please run again later.".format(lineage))
 
-    def download_lineage(self, lineage=None):
-        if lineage is None:
-            lineages = self.default_lineage
-        else:
-            lineages = [lineage]
-        for lineage in lineages:
-            if self.check_lineage(lineage):
-                continue
-            date, expected_hash = self.lineage_description[lineage][0:2]  # [date, hash_value, category]
+        if not os.path.exists(os.path.join(self.download_dir, lineage) + ".done"):
+            open(os.path.join(self.download_dir, lineage) + ".tmp", 'w').close()
+
+            try:
+                date, expected_hash = self.lineage_description[lineage][0:2]  # [date, hash_value, category]
+            except KeyError:
+                os.remove(os.path.join(self.download_dir, lineage) + ".tmp")
+                raise Error("invalid lineage name: {}".format(lineage))
+
             remote_url = self.base_url + "lineages/{}.{}.tar.gz".format(lineage, date)
             download_path = os.path.join(self.download_dir, "{}.{}.tar.gz".format(lineage, date))
             download_success = self.download_single_file(remote_url, download_path, expected_hash)
+
+            if not download_success:
+                os.remove(os.path.join(self.download_dir, lineage) + ".tmp")
+                raise Error("Unable to download necessary file {}".format("{}.{}.tar.gz".format(lineage, date)))
+
             if download_success:
                 tar = tarfile.open(download_path)
-                tar.extractall(self.download_dir)
+                # tar.extractall(self.download_dir)
+                try:
+                    tar.extractall(self.download_dir, members=[tar.getmember('{}/refseq_db.faa.gz'.format(lineage)),
+                                                               tar.getmember('{}/links_to_ODB10.txt'.format(lineage))])
+                except KeyError:
+                    tar.extractall(self.download_dir, members=[tar.getmember('{}/refseq_db.faa.gz'.format(lineage))])
+                except:
+                    os.remove(os.path.join(self.download_dir, lineage) + ".tmp")
+                    raise Error("Unable to extract file: {} or {} from {}".format("refseq_db.faa.gz",
+                                                                                  "links_to_ODB10.txt",
+                                                                                  download_path))
+
                 tar.close()
                 print("Lineage file extraction path: {}/{}".format(self.download_dir, lineage))
                 local_lineage_dir = os.path.join(self.download_dir, lineage)
                 self.lineage_description[lineage].append(local_lineage_dir)
+                open(os.path.join(self.download_dir, lineage) + ".done", 'w').close()
+                os.remove(os.path.join(self.download_dir, lineage) + ".tmp")
 
     def download_placement(self):
-        for strain in self.placement_description.keys():
-            date, expected_hash, category = self.placement_description[strain]
-            if strain.startswith("supermatrix"):
-                prefix, aln, version, sufix = strain.split(".")
-                download_file_name = "{}.{}.{}.{}.{}.tar.gz".format(prefix, aln, version, date, sufix)
-            else:
-                prefix, version, sufix = strain.split(".")
-                download_file_name = "{}.{}.{}.{}.tar.gz".format(prefix, version, date, sufix)
-            if os.path.exists(os.path.join(self.placement_dir, download_file_name)):
-                self.placement_description[strain].append(
-                    os.path.join(self.placement_dir, download_file_name.replace(".tar.gz", "")))
-                continue
-            else:
+        if os.path.exists(self.placement_dir + ".tmp"):
+            sys.exit("placement_files.tmp exists, another process is downloading, please run again later.")
+
+        if not os.path.exists(self.placement_dir + ".done"):
+            open(self.placement_dir + ".tmp", 'w').close()
+
+            for strain in self.placement_description.keys():
+                date, expected_hash, category = self.placement_description[strain]
+                if strain.startswith("supermatrix"):
+                    prefix, aln, version, sufix = strain.split(".")
+                    download_file_name = "{}.{}.{}.{}.{}.tar.gz".format(prefix, aln, version, date, sufix)
+                else:
+                    prefix, version, sufix = strain.split(".")
+                    download_file_name = "{}.{}.{}.{}.tar.gz".format(prefix, version, date, sufix)
+
                 remote_url = self.base_url + "placement_files/{}".format(download_file_name)
                 download_path = os.path.join(self.placement_dir, download_file_name)
                 download_success = self.download_single_file(remote_url, download_path, expected_hash)
+
+                if not download_success:
+                    os.remove(self.placement_dir + ".tmp")
+                    raise Error("Unable to download necessary file {}".format(download_file_name))
+
                 if download_success:
                     tar = tarfile.open(download_path)
                     tar.extractall(self.placement_dir)
@@ -178,3 +205,21 @@ class Downloader:
                                                                          download_file_name.replace(".tar.gz", "")))
                     self.placement_description[strain].append(
                         os.path.join(self.placement_dir, download_file_name.replace(".tar.gz", "")))
+            open(self.placement_dir + ".done", 'w').close()
+            os.remove(self.placement_dir + ".tmp")
+
+
+### main.py
+def main():
+    parser = argparse.ArgumentParser(description="Download specified BUSCO lineage")
+    parser.add_argument("-l", "--lineage", type=str,
+                        help="Specify the name of the BUSCO lineage to be downloaded. (e.g. eukaryota, primates, saccharomycetes etc.)",
+                        required=True)
+    parser.add_argument("-d", "--destination", type=str, help="Folder path to download folder")
+    args = parser.parse_args()
+    download = Downloader(args.destination)
+    download.download_lineage(args.lineage)
+
+
+if __name__ == "__main__":
+    main()
